7680bdd81db80c1b47d20fca98a4c50af3d3f659
Wei-Tse Sun <wtse.sun@gmail.com>
2022-08-09 17:53:10 -0700

    commit

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
 fn main() {
-    println!("Hello, world!");
+    println!("Weight on mars: {}kg", calculate_weight_on_mars(100.0));
+
+}
+
+fn calculate_weight_on_mars(weight: f32) -> f32 {
+    weight / 9.81 * 3.711
 }
